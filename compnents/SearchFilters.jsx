861d19507170fa461b14819024f6720376af057b
Caleb B <calebabaca@gmail.com>
2023-11-28 17:46:03 -0800

    error attempt

--- compnents/SearchFilters.jsx
+++ compnents/SearchFilters.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
 
-const SearchFilters = () => {
-  return (
-    <div>SearchFilters</div>
-  )
-}
+import { filterData, getFilterValues } from '../utils/FilterData';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+import noresult from //find a no result page set up image
+
+
 
 export default SearchFilters
