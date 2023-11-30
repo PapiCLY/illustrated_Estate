0431434c4f26bd36d1b1786649ba0d69615974df
Yehudah Christian <121639934+PapiCLY@users.noreply.github.com>
2023-11-30 00:55:35 -0500

    updated user routes

---
+++ routes/api/listingRoute.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const listingController = require('../controllers/listingController')
+
+router.get('/get', listingController.getListings);
+router.post('/create', listingController.createListing);
+router.delete('/delete', listingController.deleteListing);
+
+
+module.exports = router;
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/test', userController)
+const userController = require('../../controllers/userController') 
+
+router.get('/:id', userController.getUser)
+router.get('/listings/:id', userController.getUserListings)
+router.post('/update/:id', userController.updateUser)
+router.delete('/delete/:id', userController.deleteUser)
 
 module.exports = router
